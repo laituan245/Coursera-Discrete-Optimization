5c6795e38e6cdb45e3b09cf11ab606f3c7f79194
Tuan Lai <laituan245@kaist.ac.kr>
2018-12-15 23:08:07 -0500

    added typical dp sol to knapsack prob

--- knapsack/solver.py
+++ knapsack/solver.py
@@ -21,20 +21,30 @@ def solve_it(input_data):
         parts = line.split()
         items.append(Item(i-1, int(parts[0]), int(parts[1])))
 
-    # a trivial greedy algorithm for filling the knapsack
-    # it takes items in-order until the knapsack is full
-    value = 0
-    weight = 0
-    taken = [0]*len(items)
+    # ============== Dynamic Programming Approach ==============
+    dp = [[0 for i in range(capacity+1)] for j in range(len(items) + 1)]
 
-    for item in items:
-        if weight + item.weight <= capacity:
-            taken[item.index] = 1
-            value += item.value
-            weight += item.weight
-    
+
+    for i in range(1, len(items) + 1):
+        current_item = items[i-1]
+        for _capacity in range(capacity+1):
+            dp[i][_capacity] = dp[i-1][_capacity]
+            if _capacity >= current_item.weight:
+                dp[i][_capacity] = max(dp[i][_capacity],
+                                       current_item.value + dp[i-1][_capacity-current_item.weight])
+    value = dp[len(items)][capacity]
+
+    taken = [0] * len(items)
+    current_y = capacity
+    for i in range(len(items), 0, -1):
+        if dp[i][current_y] != dp[i-1][current_y]:
+            taken[i-1] = 1
+            current_y = current_y - items[i-1].weight
+
+    is_optimal = 1
+
     # prepare the solution in the specified output format
-    output_data = str(value) + ' ' + str(0) + '\n'
+    output_data = str(value) + ' ' + str(is_optimal) + '\n'
     output_data += ' '.join(map(str, taken))
     return output_data
 
@@ -48,4 +58,3 @@ if __name__ == '__main__':
         print(solve_it(input_data))
     else:
         print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
-
