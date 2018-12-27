e48b9c6d325d274866369435de7c5e54e7ad5097
Tuan Lai <laituan245@kaist.ac.kr>
2018-12-27 16:16:01 -0600

    using Google-OR tools for tsp

--- tsp/solver.py
+++ tsp/solver.py
@@ -3,11 +3,19 @@
 
 import math
 from collections import namedtuple
+from ortools.constraint_solver import pywrapcp
+from ortools.constraint_solver import routing_enums_pb2
 
 Point = namedtuple("Point", ['x', 'y'])
 
 def length(point1, point2):
     return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
+
+def create_distance_callback(points):
+    def distance_callback(from_node, to_node):
+        return int(100 * length(points[from_node], points[to_node]))
+
+    return distance_callback
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
@@ -23,22 +31,31 @@ def solve_it(input_data):
         parts = line.split()
         points.append(Point(float(parts[0]), float(parts[1])))
 
-    # build a trivial solution
-    # visit the nodes in the order they appear in the file
-    solution = range(0, nodeCount)
+    # Declare the solver
+    routing = pywrapcp.RoutingModel(len(points), 1, 0)
+    search_parameters = pywrapcp.RoutingModel.DefaultSearchParameters()
+    # Create the distance callback.
+    dist_callback = create_distance_callback(points)
+    routing.SetArcCostEvaluatorOfAllVehicles(dist_callback)
 
-    # calculate the length of the tour
-    obj = length(points[solution[-1]], points[solution[0]])
-    for index in range(0, nodeCount-1):
-        obj += length(points[solution[index]], points[solution[index+1]])
+    # Solve the problem.
+    assignment = routing.SolveWithParameters(search_parameters)
 
-    # prepare the solution in the specified output format
-    output_data = '%.2f' % obj + ' ' + str(0) + '\n'
-    output_data += ' '.join(map(str, solution))
+    if assignment:
+        results = []
+        route_number = 0
+        index = routing.Start(route_number) # Index of the variable for the starting node.
+        while not routing.IsEnd(index):
+            results.append(index)
+            index = assignment.Value(routing.NextVar(index))
 
-    return output_data
-
-
+        obj = 0.0
+        for i in range(1, len(results)):
+            obj += length(points[results[i-1]], points[results[i]])
+        obj += length(points[results[-1]], points[0])
+        output_data = '%.2f' % obj + ' ' + str(0) + '\n'
+        output_data += ' '.join(map(str, results))
+        return output_data
 import sys
 
 if __name__ == '__main__':
@@ -50,4 +67,3 @@ if __name__ == '__main__':
         print(solve_it(input_data))
     else:
         print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/tsp_51_1)')
-
