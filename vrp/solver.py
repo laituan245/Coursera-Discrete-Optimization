afe46c6e5e2736811fd7b37e8df8949d23b461c4
Tuan Lai <laituan245@kaist.ac.kr>
2018-12-27 20:04:18 -0600

    put trivial solution into a function

--- vrp/solver.py
+++ vrp/solver.py
@@ -9,34 +9,18 @@ Customer = namedtuple("Customer", ['index', 'demand', 'x', 'y'])
 def length(customer1, customer2):
     return math.sqrt((customer1.x - customer2.x)**2 + (customer1.y - customer2.y)**2)
 
-def solve_it(input_data):
-    # Modify this code to run your optimization algorithm
-
-    # parse the input
-    lines = input_data.split('\n')
-
-    parts = lines[0].split()
-    customer_count = int(parts[0])
-    vehicle_count = int(parts[1])
-    vehicle_capacity = int(parts[2])
-    
-    customers = []
-    for i in range(1, customer_count+1):
-        line = lines[i]
-        parts = line.split()
-        customers.append(Customer(i-1, int(parts[0]), float(parts[1]), float(parts[2])))
-
+def trivial_solution(customer_count, vehicle_count, vehicle_capacity, customers):
     #the depot is always the first customer in the input
-    depot = customers[0] 
+    depot = customers[0]
 
 
     # build a trivial solution
     # assign customers to vehicles starting by the largest customer demands
     vehicle_tours = []
-    
+
     remaining_customers = set(customers)
     remaining_customers.remove(depot)
-    
+
     for v in range(0, vehicle_count):
         # print "Start Vehicle: ",v
         vehicle_tours.append([])
@@ -72,6 +56,24 @@ def solve_it(input_data):
 
     return outputData
 
+def solve_it(input_data):
+    # Modify this code to run your optimization algorithm
+
+    # parse the input
+    lines = input_data.split('\n')
+
+    parts = lines[0].split()
+    customer_count = int(parts[0])
+    vehicle_count = int(parts[1])
+    vehicle_capacity = int(parts[2])
+
+    customers = []
+    for i in range(1, customer_count+1):
+        line = lines[i]
+        parts = line.split()
+        customers.append(Customer(i-1, int(parts[0]), float(parts[1]), float(parts[2])))
+
+    return trivial_solution(customer_count, vehicle_count, vehicle_capacity, customers)
 
 import sys
 
@@ -85,4 +87,3 @@ if __name__ == '__main__':
     else:
 
         print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/vrp_5_4_1)')
-
